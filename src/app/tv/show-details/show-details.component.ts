83087ad71c0f022bb97e5078b845883bf07dd68d
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-23 15:21:56 +0100

    episodise filter

--- src/app/tv/show-details/show-details.component.ts
+++ src/app/tv/show-details/show-details.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute} from '@angular/router';
 import {ShowDetailsParams} from '../../app-routing.module';
 import {ShowDetails} from '../tv.models';
 import {TvMazeService} from '../tv-maze.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'tm-show-details',
@@ -11,10 +13,14 @@ import {TvMazeService} from '../tv-maze.service';
 })
 export class ShowDetailsComponent implements OnInit {
   show: ShowDetails;
+  episodesLength = 2;
 
   constructor(private route: ActivatedRoute) {
     // this.route.params.subscribe(p => console.log(p));
     this.show = this.route.snapshot.data.show;
+    if (this.show._embedded.episodes.some(({number}) => number > 99)) {
+      this.episodesLength = 3;
+    }
   }
 
   ngOnInit() {
