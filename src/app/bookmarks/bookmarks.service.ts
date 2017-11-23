b5bebc34d94dafda14976ef6a408dbbc956355c0
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-23 10:36:19 +0100

    saving bookmarks to backend (json-server)

--- src/app/bookmarks/bookmarks.service.ts
+++ src/app/bookmarks/bookmarks.service.ts
@@ -1,12 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Bookmark, BookmarkId} from './bookmarks.models';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class BookmarksService {
   version = 1;
   private items: Bookmark[] = [];
+  private readonly apiBaseUrl = 'http://localhost:3000/bookmarks';
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.http.get<Bookmark[]>(this.apiBaseUrl)
+      .subscribe(items => this.items = items);
   }
 
   getAll(): Bookmark[] {
@@ -14,11 +18,17 @@ export class BookmarksService {
   }
 
   add(item: Bookmark): void {
-    this.items = [...this.items, item];
+    this.http.post(this.apiBaseUrl, item)
+      .subscribe(() => {
+        this.items = [...this.items, item];
+      });
   }
 
   remove(id: BookmarkId): void {
-    this.items = this.items.filter(item => item.id !== id);
+    this.http.delete(`${this.apiBaseUrl}/${id}`)
+      .subscribe(() => {
+        this.items = this.items.filter(item => item.id !== id);
+      });
   }
 
   has(id: BookmarkId): boolean {
