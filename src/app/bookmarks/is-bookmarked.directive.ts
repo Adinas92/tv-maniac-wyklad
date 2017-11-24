c1b80764081e4e042a564bdbd88469522d4cce09
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-24 09:21:02 +0100

    isBookmarked directive

--- src/app/bookmarks/is-bookmarked.directive.ts
+++ src/app/bookmarks/is-bookmarked.directive.ts
@@ -1,12 +1,29 @@
-import { Directive } from '@angular/core';
+import {Directive, HostBinding, HostListener, Input} from '@angular/core';
+import {BookmarksService} from './bookmarks.service';
+import {Bookmark} from './bookmarks.models';
 
 @Directive({
   selector: '[tmIsBookmarked]'
 })
 export class IsBookmarkedDirective {
+  @Input('tmIsBookmarked') item: Bookmark; // tslint:disable-line:no-input-rename
 
-  constructor() {
-    console.log('Hello from directive');
+  @HostBinding('class.opaque')
+  isOpaque = false;
+
+  constructor(private bs: BookmarksService) {
+  }
+
+  @HostBinding('class.bookmarked')
+  get isBookmarked(): boolean {
+    return this.bs.has(this.item.id);
+  }
+
+  @HostListener('click', ['$event.target'])
+  toggleOpaque(target: HTMLElement) {
+    if (target.tagName === 'IMG') {
+      this.isOpaque = !this.isOpaque;
+    }
   }
 
 }
